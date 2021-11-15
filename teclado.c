28efc2ba1006f1fef5e1335ec42613323ae7b7d0
PabloCBEG <89579608+PabloCBEG@users.noreply.github.com>
2021-11-15 22:38:26 +0100

    Add files via upload

--- teclado.c
+++ teclado.c
@@ -30,30 +30,33 @@ const int32_t REG_CAL5[6]={32146, -1428, -331110, -40, -18930, 18321010};
 #define NUM_SSI_DATA 3
 #define MSEC 40000
 
-int RELOJ;
+int RELOJ, bkspc, letra;
 
-void keys(int16_t x, int16_t y, int16_t w, int16_t h, int16_t font, uint16_t options,const char* s)
+//----------------------------------------DECLARACION DE FUNCIONES-------------------------------------------------------------------
+
+void keys(int16_t x, int16_t y, int16_t w, int16_t h, int16_t font, uint16_t options, const char* s)
 {
     //funcion que dibuja un teclado
     int largo = strlen(s);
-    int contador_cadena, xaux;
+    int contador_cadena, xaux, logico;
     char aux;
-
-    if(largo%2==0) xaux = HSIZE/2-(largo/2)*w;
-    else xaux = HSIZE/2-((largo/2)*w+w/2);
 
     if(options==OPT_CENTER)
     {
-        if(largo%2==0) xaux = HSIZE/2-(largo/2)*w;
-        else xaux = HSIZE/2-((largo/2)*w+w/2);
-    }
-    else xaux=x;
+        if(largo%2==0)  xaux = HSIZE/2-(largo/2)*(w+3);         //3 pixels es el espacio que queremos dejar entre tecla y tecla. Lo ideal seria que
+        else            xaux = HSIZE/2-((largo/2)*(w+3)+w/2);   //se lo pasaramos como parametro y la funcion hiciera sus calculos. Pendiente
+    }                                                           //de incorporarlo en la version definitiva
+    else                xaux = x;
 
+    logico=0;
     for(contador_cadena=0;contador_cadena<largo;contador_cadena++)
     {
         strncpy(&aux,s+contador_cadena,1);
-        Boton(xaux+contador_cadena*w, y, w, h, font, &aux);//&aux[contador_cadena]);//*logico (pal 3 de la coord x)
-        UARTprintf("%c", aux);
+//        logico = 0;
+//        if (contador_cadena > 0) logico=1;
+        Boton(xaux+contador_cadena*logico*(w+3), y, w, h, font, &aux);
+        logico = 1;
+//        UARTprintf("%c", aux);
     }
 }
 
@@ -63,10 +66,15 @@ void teclado(int16_t vm)
     if(vm==35) font=26;
     else font=27;
 
-    keys(3,VSIZE/2,HSIZE/10,VSIZE/8,font,OPT_CENTER,"QWERTYUIOP");
-    keys(3,VSIZE/2+VSIZE/8+3,HSIZE/10,VSIZE/8,font,OPT_CENTER,"ASDFGHJKL");
-    keys(3,VSIZE/2+2*VSIZE/8+6,HSIZE/10,VSIZE/8,font,OPT_CENTER,"ZXCVBNM");
-    Boton(HSIZE/2-60,VSIZE/2+3*VSIZE/8+9,120,VSIZE/8,font,"");
+    keys(3,VSIZE/2,(HSIZE-30)/10,(VSIZE/2-12)/4,font,OPT_CENTER,"QWERTYUIOP");
+    keys(3,VSIZE/2+(VSIZE/2-12)/4+3,(HSIZE-30)/10,(VSIZE/2-12)/4,font,OPT_CENTER,"ASDFGHJKL");
+    keys(3,VSIZE/2+2*(VSIZE/2-12)/4+6,(HSIZE-30)/10,(VSIZE/2-12)/4,font,OPT_CENTER,"ZXCVBNM");
+    Boton(HSIZE/2-60,VSIZE/2+3*(VSIZE/2-12)/4+9,120,(VSIZE/2-12)/4,font,"");
+    Boton(HSIZE-46,VSIZE/2+2*(VSIZE/2-12)/4+6,43,(VSIZE/2-12)/4,font,"<---");
+
+//    keys(3,VSIZE/2,HSIZE/10,VSIZE/8,font,OPT_CENTER,"ALEJANDRO");
+//    keys(3,VSIZE/2+VSIZE/8+3,HSIZE/10,VSIZE/8,font,OPT_CENTER,"ES TELA");
+//    keys(3,VSIZE/2+2*VSIZE/8+6,HSIZE/10,VSIZE/8,font,OPT_CENTER,"DE MARICA");
 }
 
 void main(){
@@ -93,10 +101,11 @@ void main(){
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     GPIOPinConfigure(GPIO_PA0_U0RX);
     GPIOPinConfigure(GPIO_PA1_U0TX);
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);//
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     UARTStdioConfig(0, 115200, RELOJ);
 
 //-------------------------------PROGRAMA-------------------------------------------------------------
+
 
     while(1){
     //pintamos pantalla inicial
@@ -104,6 +113,8 @@ void main(){
         Lee_pantalla();
         Nueva_pantalla(16,16,16);
 
+        ComFgcolor(0,122,116);
+        ComColor(255, 255, 255);
         teclado(35);
 
         Dibuja();
