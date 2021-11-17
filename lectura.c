b7875c446343b447e53311e8ffa328722b0935fe
AlejandroTorresMunoz <aletm170399@gmail.com>
2021-11-17 16:52:19 +0100

    Update lectura.c
    Ahora lectura.c puede leer archivos de texto de varias lineas. Se puede tomar como ejemplo para la implementación de una futura función.
    
    Importante: Se debe aumentar el tamaño de la HEAP, ya que las funciones tipo fgets,fscanf... Hacen uso de ese tipo de memoria dinámica, por lo que se debe aumentar el tamaño en el proyecto. Poniendo a 400, por ahora, funciona.

--- lectura.c
+++ lectura.c
@@ -1,43 +1,125 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
+
+
 #include "driverlib2.h"
 #include "utils/uartstdio.h"
-#include "sensorlib2.h"
-//asqui lo que hace falta es pasar por serie lectura teclado pa indicar paso de renglon, y sacar por serie UART el renglon indicado
-//
 
-FILE *fopen( const char * filename, const char * mode );//DEFINICION DE TIPO FILE
-int RELOJ;
+//#include "HAL_I2C.h"
+//#include "sensorlib2.h"
+/***********************************
+ * Ejemplo de manejo del PWM. Se configura para que un servo en PF1 cambie
+ * Mientras se tenga pulsado un botón, cambia la consigna del servo
+ * Se usa para ello el PWM1, con un periodo de 50Hz, y un duty cycle entre 1ms y 2ms aprox
+ * Se usan variables globales para poder cambiarlo en GUiComposer
+ *************************************/
 
-void main(){
+#define MSEC 40000
+#define MaxEst 10
+
+
+#define B1_OFF GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_0)
+#define B1_ON !(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_0))
+#define B2_OFF GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_1)
+#define B2_ON !(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_1))
+
+
+volatile int Max_pos = 4200; //3750
+volatile int Min_pos = 1300; //1875
+
+int RELOJ, PeriodoPWM;
+volatile int pos_100, pos_1000;
+volatile int posicion;
+
+int estado;
+uint32_t periodo_global;
+uint32_t periodo_global_100ms;
+
+
+
+
+void IntTimer0(void)
+{
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT); // Borra flag
+
+    //SysCtlDelay(10*MSEC);
+    estado = 3;
+    //SysCtlDelay(10*MSEC);
+
+
+
+}
+
+
+int main(void)
+{
+
+
     RELOJ=SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
 
-    //habilitacion y configuracion de la uart
+
+
+
+    //Configuracion del mperiferioc UART
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     GPIOPinConfigure(GPIO_PA0_U0RX);
     GPIOPinConfigure(GPIO_PA1_U0TX);
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);//
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+
+
     UARTStdioConfig(0, 115200, RELOJ);
 
-        int i;
-        FILE *fp;
-        char buff[255];
-        char texto[255][255];
-        fp = fopen("uno.txt","r");
-    //    linea = *fgets();
-        i=0;
-        while(fgets(buff, sizeof(buff), (FILE*)fp)){
-            //printf("%s\n", buff); //para imprimir todas las lineas del txt
+
+    IntMasterEnable();
+
+
+    UARTprintf("Fin de la configuracion.\n");
+
+    FILE *fp;
+    fp = fopen("uno.txt","r");
+
+    char buff[200];
+    char texto[5][200];
+    int i,j;
+    UARTprintf("Fichero de texto abierto.\n");
+    if (fp == NULL)
+    {
+        UARTprintf("Error en la apertura del fichero.\n");
+        exit(1);
+    }
+    else
+    {
+        UARTprintf("Apertura realizada con exito.\n");
+        i = 0; //Para ir contando
+        while (feof(fp) == 0)
+        {
+            fgets(buff,200,fp);
+            UARTprintf("%s",buff);
             strcpy(texto[i],buff);
             i++;
         }
-        for(i=0;i<8;i++)
-        {
-        UARTprintf("%s\n", texto[i]);
-        }
-        fclose(fp);
+
+        //fscanf(fp, "%s" ,buffer);
+        //UARTprintf("%s\n",buffer);
+        //UARTprintf("%s\n",buff);
+    }
+    UARTprintf("\n");
+    UARTprintf("Fin de la lectura del archivo.\n");
+    for(j=0;j<i;j++)
+    {
+        UARTprintf("%s\n",texto[j]);
+    }
+    fclose(fp);
+
+
+
+    UARTprintf("Fin de la ejecucion.\n");
+    return 0;
 }
+
