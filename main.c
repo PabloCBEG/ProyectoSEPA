f5defb74abbbb42cb91f3f7f621d1bdd889f8a6a
Alejandro Torres Muñoz <75946946+AlejandroTorresMunoz@users.noreply.github.com>
2021-12-23 22:17:38 +0100

    Creación main.c
    Ahora el microprocesador es capaz de leer un registro del periférico wishbone. Luego se lee lo introducido por el teclado y se compara.

--- main.c
+++ main.c
@@ -40,8 +40,9 @@
  **************************************************************************/
 
 #include <neorv32.h>
-
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**********************************************************************//**
  * @name User configuration
  **************************************************************************/
@@ -55,6 +56,7 @@
 #define NUM_COLUMNAS 4 //Numero de columnas del teclado
 #define NUM_STRINGS  16
 #define MAX_LENGTH  100
+
 
 
 /**********************************************************************//**
@@ -75,6 +77,9 @@ void blink_led_c(void);
  *
  * @return 0 if execution was successful
  **************************************************************************/
+uint32_t funcion_teclado(void);
+
+
 int main() {
 
   // init UART (primary UART = UART0; if no id number is specified the primary UART is used) at default baud rate, no parity bits, ho hw flow control
@@ -91,7 +96,16 @@ int main() {
   neorv32_rte_setup();
 
   // say hello
-  neorv32_uart0_print("Practica 2 extendido.\n");
+  neorv32_uart0_print("Lectura de registros wishbone.\n");
+
+int i,j;
+uint32_t cont = 1;
+uint32_t direccion_lectura; //Direccion base del periferico wishbone al que queremos acceder
+uint32_t valor_leido;
+
+int columna;
+int fila;
+neorv32_uart0_printf("El valor de cont en la primera vuelta es : %u\n",cont);
 // use ASM version of LED blinking (file: blink_led_in_asm.S)
 #ifdef USE_ASM_VERSION
 
@@ -99,66 +113,27 @@ int main() {
 
 // use C version of LED blinking
 #else
-uint32_t valores_teclado; //Variable para la lectura de los pines del gpio
-uint32_t cont = 15;
-uint32_t mascaras[4] = {0x000f,0x00f0,0x0f00,0xf000}; //Mascaras a aplicar
-uint32_t valores_fila_lectura[4];
-uint32_t valores_fila_lectura_anterior[4];
-
-char arr1[NUM_STRINGS][MAX_LENGTH] = {"NADA","1","4","1 y 4","7","1 y 7","4 y 7","1 y 4 y 7","0","1 y 0","4 y 0","1 y 4 y 0","7 y 0","1 y 7 y 0","4 y 7 y 0","1 y 4 y 7 y 0"};
-char arr2[NUM_STRINGS][MAX_LENGTH] = {"NADA","2","5","2 y 5","8","2 y 8","5 y 8","2 y 5 y 8","F","2 y F","5 y F","2 y 5 y F","8 y F","2 y 8 y F","5 y 8 y F","2 y 5 y 8 y F"};
-char arr3[NUM_STRINGS][MAX_LENGTH] = {"NADA","3","6","3 y 6","9","3 y 9","6 y 9","3 y 6 y 9","E","3 y E","6 y E","3 y 6 y E","9 y E","3 y 9 y E","6 y 9 y E","3 y 6 y 9 y E"};
-char arr4[NUM_STRINGS][MAX_LENGTH] = {"NADA","A","B","A y B","C","A y C","B y C","A y B y C","D","A y D","B y D","A y B y D","C y D","A y C y D","B y C y D","A y B y C y D"};
-
-
-int i; //Variable auxiliar para el bucle
 while(1)
 {
-    valores_teclado = neorv32_gpio_port_get(); //Obtencion del vector de valores del teclado. Solo nos interesan los 16 menos significativos
-    valores_teclado = ~valores_teclado; //Se niega. Por lo tanto, si es un 1, significa que esta pulsado, ya que antes era activa a nivel bajo.
-    //valores_teclado = valores_teclado << 4;
-    //neorv32_uart0_printf("Valor de valores_teclado : %x\n",valores_teclado);
-    cont = 15;
-    for(i=0;i<4;i++)
-    {
-        valores_fila_lectura[i] = valores_teclado & mascaras[i];
-        valores_fila_lectura[i] = valores_fila_lectura[i] >>(4*i);
-        //neorv32_uart0_printf("Valor de valores_fila_lectura : %x\n",valores_fila_lectura);
-        switch(i)
-        {
-            case 0:
-                if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i])
-                {
-                    neorv32_uart0_printf("Se ha pulsado : %s.\n",arr1[valores_fila_lectura[i]]);
-                }
-                break;
-            case 1:
-                if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i])
-                {
-                    neorv32_uart0_printf("Se ha pulsado : %s.\n",arr2[valores_fila_lectura[i]]);
-                }
-                break;
-            case 2:
-                if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i]) 
-                {
-                    neorv32_uart0_printf("Se ha pulsado : %s.\n",arr3[valores_fila_lectura[i]]);
-                }
-                break;
-            case 3:
-                if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i])
-                {
-                    neorv32_uart0_printf("Se ha pulsado : %s.\n",arr4[valores_fila_lectura[i]]);
-                }
-                break;
-            default:
-                break;
-        }
-        valores_fila_lectura_anterior[i] = valores_fila_lectura[i];
-    }
-        
-
-
+    direccion_lectura = (uint32_t)0x90000000;
+    neorv32_uart0_printf("Se va a leer la contrasena.\n");
+    valor_leido = neorv32_cpu_load_unsigned_word(direccion_lectura);
+    neorv32_uart0_printf("El valor de la contrasena es : %x.\n",valor_leido);
+    neorv32_uart0_printf("Por favor, introduzca el valor de la contrasena por teclado.\n");
     
+    uint32_t contrasena_introducida = funcion_teclado();
+    neorv32_uart0_printf("Se ha introducido la siguiente contrasena : %x.\n",contrasena_introducida);
+
+  if(contrasena_introducida == valor_leido)
+  {
+    neorv32_uart0_printf("Enhoranbuena. Correcto.\n");
+  }
+  else
+  {
+    neorv32_uart0_printf("Pruebe otra vez.\n");
+  }
+
+
 }
 
 
@@ -170,4 +145,127 @@ while(1)
 /**********************************************************************//**
  * C-version of blinky LED counter
  **************************************************************************/
+ /*
+void blink_led_c(void) {
+
+  neorv32_gpio_port_set(0); // clear gpio output
+
+  int cnt = 0;
+
+  while (1) {
+    neorv32_gpio_port_set(cnt++ & 0xFF); // increment counter and mask for lowest 8 bit
+    neorv32_cpu_delay_ms(200); // wait 200ms using busy wait
+  }
+}
+
+*/
+
+uint32_t funcion_teclado(void)
+{
+  uint32_t valores_teclado; //Variable para la lectura de los pines del gpio
+  uint32_t cont = 15;
+  uint32_t mascaras[4] = {0x000f,0x00f0,0x0f00,0xf000}; //Mascaras a aplicar
+  uint32_t valores_fila_lectura[4];
+  uint32_t valores_fila_lectura_anterior[4];
+
+  char arr1[NUM_STRINGS][MAX_LENGTH] = {"NADA","1","4","1 y 4","7","1 y 7","4 y 7","1 y 4 y 7","0","1 y 0","4 y 0","1 y 4 y 0","7 y 0","1 y 7 y 0","4 y 7 y 0","1 y 4 y 7 y 0"};
+  char arr2[NUM_STRINGS][MAX_LENGTH] = {"NADA","2","5","2 y 5","8","2 y 8","5 y 8","2 y 5 y 8","F","2 y F","5 y F","2 y 5 y F","8 y F","2 y 8 y F","5 y 8 y F","2 y 5 y 8 y F"};
+  char arr3[NUM_STRINGS][MAX_LENGTH] = {"NADA","3","6","3 y 6","9","3 y 9","6 y 9","3 y 6 y 9","E","3 y E","6 y E","3 y 6 y E","9 y E","3 y 9 y E","6 y 9 y E","3 y 6 y 9 y E"};
+  char arr4[NUM_STRINGS][MAX_LENGTH] = {"NADA","A","B","A y B","C","A y C","B y C","A y B y C","D","A y D","B y D","A y B y D","C y D","A y C y D","B y C y D","A y B y C y D"};
+
+  uint32_t contrasena_escrita; //Valor que se va a devolver
+  char string_teclas_pulsadas[MAX_LENGTH]; //Cadena que ira almacenando el valor de la tecla que se pulsa por teclado
+  int longitud_teclas_pulsadas;
+  int cont_contrasena = 0;
+
+  int i; //Variable auxiliar para el bucle
+  int flag_escritura_terminada = 0;
+  while(flag_escritura_terminada == 0)
+  {
+      longitud_teclas_pulsadas = 0;
+      valores_teclado = neorv32_gpio_port_get(); //Obtencion del vector de valores del teclado. Solo nos interesan los 16 menos significativos
+      valores_teclado = ~valores_teclado; //Se niega. Por lo tanto, si es un 1, significa que esta pulsado, ya que antes era activa a nivel bajo.
+      //valores_teclado = valores_teclado << 4;
+      //neorv32_uart0_printf("Valor de valores_teclado : %x\n",valores_teclado);
+
+      for(i=0;i<4;i++)
+      {
+          valores_fila_lectura[i] = valores_teclado & mascaras[i];
+          valores_fila_lectura[i] = valores_fila_lectura[i] >>(4*i);
+          //neorv32_uart0_printf("Valor de valores_fila_lectura : %x\n",valores_fila_lectura);
+          switch(i)
+          {
+              case 0:
+                  if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i])
+                  {
+                      neorv32_uart0_printf("Se ha pulsado : %s.\n",arr1[valores_fila_lectura[i]]);
+                      strcpy(string_teclas_pulsadas,arr1[valores_fila_lectura[i]]);
+                      longitud_teclas_pulsadas = strlen(string_teclas_pulsadas);
+                  }
+                  break;
+              case 1:
+                  if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i])
+                  {
+                      neorv32_uart0_printf("Se ha pulsado : %s.\n",arr2[valores_fila_lectura[i]]);
+                      strcpy(string_teclas_pulsadas,arr2[valores_fila_lectura[i]]);
+                      longitud_teclas_pulsadas = strlen(string_teclas_pulsadas);
+                  }
+                  break;
+              case 2:
+                  if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i]) 
+                  {
+                      neorv32_uart0_printf("Se ha pulsado : %s.\n",arr3[valores_fila_lectura[i]]);
+                      strcpy(string_teclas_pulsadas,arr3[valores_fila_lectura[i]]);
+                      longitud_teclas_pulsadas = strlen(string_teclas_pulsadas);
+                  }
+                  break;
+              case 3:
+                  if(valores_fila_lectura[i] > 0 && valores_fila_lectura[i] != valores_fila_lectura_anterior[i])
+                  {
+                      neorv32_uart0_printf("Se ha pulsado : %s.\n",arr4[valores_fila_lectura[i]]);
+                      strcpy(string_teclas_pulsadas,arr4[valores_fila_lectura[i]]);
+                      longitud_teclas_pulsadas = strlen(string_teclas_pulsadas);
+                  }
+                  break;
+              default:
+                  break;
+          }
+        valores_fila_lectura_anterior[i] = valores_fila_lectura[i];
+      }
+
+      if(longitud_teclas_pulsadas == 1)
+      {
+        //int aux = string_teclas_pulsadas[0]-'0'; //Lo convertimos de caracter a entero
+        uint32_t aux = strtol(&string_teclas_pulsadas[0],NULL,16);
+        uint32_t aux2 = aux;
+        neorv32_uart0_printf("El valor de aux2 es : %x.\n",aux2);
+        uint32_t aux3 = aux2<<(28-(4*cont_contrasena));
+        neorv32_uart0_printf("El valor de aux3 es : %x.\n",aux3);
+        contrasena_escrita = contrasena_escrita+aux3;
+        cont_contrasena = cont_contrasena+1;
+      }
+      else if(longitud_teclas_pulsadas > 1)
+      {
+        neorv32_uart0_printf("Se han pulsado demasiadas teclas a la vez.\n");
+      }
+
+      if(cont_contrasena == 8)
+      {
+        flag_escritura_terminada = 1;
+      }
+      else if(cont_contrasena > 8)
+      {
+        neorv32_uart0_printf("cont_contrasena = %x.\n",cont_contrasena);
+      }
+          
+      //Parte encarga de dar valores a la senal del controlador
+      //Recordamos que debemos escribir sobre los 7 pines significativos del controlador el valor
+      //neorv32_gpio_port_set(porcentaje_servo);
+      //neorv32_uart0_printf("El valor de porcentaje servo es : %x.\n",porcentaje_servo);
+
+      
+  }
+
+  return contrasena_escrita;
+}
 
