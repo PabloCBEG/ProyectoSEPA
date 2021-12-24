98c84b3ebd02a9b5f2b7ae1909df1ce787730993
PabloCBEG <89579608+PabloCBEG@users.noreply.github.com>
2021-12-24 17:31:30 +0100

    Ahora sí, comentarios un poco en serio sobre el controlador del servo

--- controlador_servo.vhd
+++ controlador_servo.vhd
@@ -4,7 +4,7 @@
 -- 
 -- Create Date: 20/12/2021
 -- Design Name: 
--- Module Name: debouncer - Behavioral
+-- Module Name: Controlador servo
 
 -- 
 ----------------------------------------------------------------------------------
@@ -13,17 +13,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use IEEE.MATH_REAL.ALL;
 
---Entidad del debouncer
+--Entidad del controlador del servomotor
 entity controlador_servo is
     Generic (
-     numero_pines  : integer; --Numero de pines de servo(4)
-     pin_conectado : integer  --Pin al que conectamos nuestra salida
+        numero_pines  : integer; --Numero de pines de servo(4)
+        pin_conectado : integer  --Pin al que conectamos nuestra salida
     );
     Port ( 
-    clk : in std_logic; --Puerto de reloj
-    reset : in std_logic; --Puerto de reseteo
-    entrada_microsegundos : in std_logic_vector(10 downto 0); --Porcentaje al que queremos que se mueva el servo
-    port_output : out std_logic_vector(numero_pines-1 downto 0) --Boton de salida
+        clk                     : in std_logic;                                 --Pin de reloj
+        reset                   : in std_logic;                                 --Pin de reseteo
+        entrada_microsegundos   : in std_logic_vector(10 downto 0);             --Porcentaje al que queremos que se mueva el servo
+        port_output             : out std_logic_vector(numero_pines-1 downto 0) --Boton de salida
     );
 end controlador_servo;
 
@@ -32,40 +32,38 @@ end controlador_servo;
 architecture Behavioral of controlador_servo is
 
 
+constant maximo_pulsos : integer := 27600;  --2300us * 12pulsos/us (pues la freq del micro es 12MHz == 12*10⁶ ciclos/s)
+constant pulsos_offset : integer := 17220;  --Valor calculado experimentalmente: 1500us * 12pulsos/us. Pero se comprueba que para 1500*12 el servo no se queda quieto. Por tanto se ha ido probando con distintos valores hasta que se ha dado con el adecuado.
+                                            --Es 1435us * 12pulsos/us
 
-constant maximo_pulsos : integer := 27600;
-constant pulsos_offset : integer := 17220;
+signal pulse_width  : integer := pulsos_offset; --Numero de pulsos en alto
+signal contador     : integer := 0;
+signal aux          : integer := 0;
 
-
-
-signal pulse_width : integer := pulsos_offset; --Numero de pulsos en alto
-signal contador : integer := 0;
-signal aux : integer := 0;
-
---Comienzo del comportamiento
+--Comienzo del comportamiento / behavioral
 begin
 
     process(clk,reset)
     begin
         if reset = '0' then --Si reseteamos, todo a 0
-            contador <= 0;
+            contador    <= 0;
             pulse_width <= pulsos_offset;
             port_output <= std_logic_vector(to_unsigned(0,numero_pines));
-            aux <= 0;
+            aux         <= 0;
             
         elsif rising_edge(clk) then --Si no reseteamos, si hay flanco ascendente de reloj
             if(contador = maximo_pulsos-1) then 
                 contador <= 0;
             else
                 if(contador = 0) then --Si estamos en el primer ciclo de reloj, actualizamos el valor del pulse_width
-                    aux <= (to_integer(signed(entrada_microsegundos))*12);
+                    aux         <= (to_integer(signed(entrada_microsegundos))*12);
                     pulse_width <= pulsos_offset+aux; 
                 else
                     
                     if(contador < pulse_width) then
                         for i in 0 to numero_pines-1 loop
                             if (i = pin_conectado) then --Escribimos solo el valor alto sobre el pin del servo
-                                port_output(i) <= '1'; --En el pin del servo, valor alto
+                                port_output(i) <= '1';  --En el pin del servo, valor alto
                             else
                                 port_output(i) <= '0';
                             end if;
